526a7290cc11ff77afd91d973d620859c6d20ff1
Thom Palmer <thomapalmer@gmail.com>
2013-10-11 21:28:11 -0400

    One method. GetTransactionTypeID

---
+++ Assignment1ATM/Assignment1ATM/TransactionType.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1ATM
+{
+    class TransactionType
+    {
+        private string connectionString = ConnectionString.ATMConnectionString;
+       
+        public int TransactionTypeId
+        {
+            get;
+            set;
+        }
+
+        public string Transaction
+        {
+            get;
+            set;
+        }
+
+        public TransactionType()
+        {
+            TransactionTypeId = 0;
+            Transaction = string.Empty;
+        }
+
+        public int GetTransactionTypeId(string Transaction)
+        {
+            string sqlQuery = "SELECT * FROM TransactionType WHERE Transaction = '" + Transaction + "'";
+
+            SqlConnection connection = new SqlConnection(this.connectionString);
+            SqlCommand command = new SqlCommand(sqlQuery, connection);
+            SqlDataReader reader = null;
+
+            try
+            {
+                connection.Open();
+                reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    return int.Parse(reader["TransactionTypeId"].ToString());
+                }
+                else 
+                {
+                    Console.Out.WriteLine("Invalid Transaction Type");
+                    return 0;
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
